6bc9e9e2776776a0bb52e4b6dd761c2d44a55117
zhengzhiwen <zhengzhiwen@cnncpm.com>
2024-02-20 09:06:24 +0800

    fix bug: TCPFeedbackInit

--- ArmCoreV1_QSPI/application/config.h
+++ ArmCoreV1_QSPI/application/config.h
@@ -2,6 +2,6 @@
 #define __CONFIG_H__
 
 #define GIT_BRANCH  "arm_core_master_restructure_temporary"
-#define GIT_HASH    "3ec97cc133e991e4545dc933c4b8734f792545b3"
+#define GIT_HASH    "81372bb53f2ad639991e270878a80fecb7b36318"
 
 #endif
--- ArmCoreV1_QSPI/application/nonRealtimeDataProcess.c
+++ ArmCoreV1_QSPI/application/nonRealtimeDataProcess.c
@@ -106,14 +106,20 @@ bool InitCrc32Table(void);
 void sendParamtoFPGA(void);
 void calCarrierTrajectory(void);
 
-uint16_t *feedback, feedback16Len;
+uint16_t *feedback = NULL, feedback16Len;
 
-uint16_t buf[186];
 void TCPFeedbackInit(void)
 {
     feedback16Len = (3+sizeof(interlockFeedback)/2) + (3+sizeof(secondPosFeedback)/2);
-    feedback = (uint16_t*)buf;//pvPortMalloc(feedback16Len);
-    memset(feedback, 0, feedback16Len*2);
+    feedback = (uint16_t *)pvPortMalloc(feedback16Len*2);
+    if (feedback != NULL)
+    {
+        memset(feedback, 0, feedback16Len*2);
+    }
+    else
+    {
+        printf("%s file malloc err(line:%d)\r\n", __FILE__, __LINE__);
+    }    
 }
 
 uint16_t beam_cmd_get(void)
