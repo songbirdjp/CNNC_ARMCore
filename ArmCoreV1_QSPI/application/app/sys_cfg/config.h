d70a24c3fd7969dcc3226b28cd043cd57333aaaf
zhengzhiwen <zhengzhiwen@cnncpm.com>
2024-07-05 14:21:54 +0800

    upgrade stm32cubemx version to 6.12.0

--- ArmCoreV1_QSPI/application/app/sys_cfg/config.h
+++ ArmCoreV1_QSPI/application/app/sys_cfg/config.h
@@ -2,6 +2,6 @@
 #define __CONFIG_H__
 
 #define GIT_BRANCH  "arm_core_master_restructure_temporary"
-#define GIT_HASH    "91c1f318bf22aff075a75dae82d3d270160f82b0"
+#define GIT_HASH    "847fee91f8ed8199dd6c9467d3feeef8748c5c87"
 
 #endif
