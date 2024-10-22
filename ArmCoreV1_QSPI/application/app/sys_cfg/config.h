8a9ee2cf22356fcc6316d473934dd34038f63918
zhengzhiwen <zhengzhiwen@cnncpm.com>
2024-10-22 14:19:09 +0800

    optimize driver uart, to solve ne/fe error

--- ArmCoreV1_QSPI/application/app/sys_cfg/config.h
+++ ArmCoreV1_QSPI/application/app/sys_cfg/config.h
@@ -2,6 +2,6 @@
 #define __CONFIG_H__
 
 #define GIT_BRANCH  "arm_core_master_restructure_temporary"
-#define GIT_HASH    "ffd0a05360cc90d46303b39648ce74e65e7c3fab"
+#define GIT_HASH    "b4fd51fc36db05a043abc6f3fdc51bb6bdfdc46e"
 
 #endif
