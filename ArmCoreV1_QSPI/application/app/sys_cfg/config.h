bc8664b6e1be8f18bcafa58312eda28ae74029b7
caijiaheng <caijiaheng@cnncpm.com>
2024-10-30 10:02:59 +0800

    add WSConsole.html to remote debug

--- ArmCoreV1_QSPI/application/app/sys_cfg/config.h
+++ ArmCoreV1_QSPI/application/app/sys_cfg/config.h
@@ -2,6 +2,6 @@
 #define __CONFIG_H__
 
 #define GIT_BRANCH  "bgm_afc_develop"
-#define GIT_HASH    "0a58d474cd1c3a011eb896d0b2107fd577ab7af0"
+#define GIT_HASH    "567d69f7608a51f49a42c360f3ec1f2609ab8a4a"
 
 #endif
