cb7e335626151068037a3b97538f6c8cde123625
zhengzhiwen <zhengzhiwen@cnncpm.com>
2025-05-07 17:20:27 +0800

    1. optimize external wdt control 2. add delay for ethercat thread initial

--- ArmCoreV1_QSPI/application/app/sys_cfg/config.h
+++ ArmCoreV1_QSPI/application/app/sys_cfg/config.h
@@ -2,6 +2,6 @@
 #define __CONFIG_H__
 
 #define GIT_BRANCH  "bgm_arm_io_develop"
-#define GIT_HASH    "1e6b5199f73aa5d991a073e0fc6a81b70e16aefb"
+#define GIT_HASH    "2b0e244f4b21901c4f9f02eed9e7999a8d40e16c"
 
 #endif
