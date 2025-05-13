c23f01961aa7e118abdec61eeb67be8f6fff005f
siyunlong <siyunlong@cnncpm.com>
2025-05-13 14:57:55 +0800

    更新.ioc文件fkp速率到500k

--- ArmCoreV1_QSPI/application/app/sys_cfg/config.h
+++ ArmCoreV1_QSPI/application/app/sys_cfg/config.h
@@ -2,6 +2,6 @@
 #define __CONFIG_H__
 
 #define GIT_BRANCH  "rtm_off_arm_io_develop_new"
-#define GIT_HASH    "c2a174a75ec6d3e18bb281ca3a1442208af26bbb"
+#define GIT_HASH    "9d3c46bb96435a77d46df65210e60655948a6566"
 
 #endif
